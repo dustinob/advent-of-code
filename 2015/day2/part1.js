0ff3f6f07b86b584cf63579c970f1a7c4ab8dc84
Dustin O'Brien <dcobrien82@gmail.com>
2017-05-02 21:22:28 -0700

    Day 2 - Part 1 Complete

--- 2015/day2/part1.js
+++ 2015/day2/part1.js
@@ -3,4 +3,29 @@
 const fs = require('fs');
 const INPUT = fs.readFileSync('./input.txt').toString().split('\n');
 
-console.log(INPUT);
+// dim: 4x23x21 LxWxH
+// 2*l*w + 2*w*h + 2*h*l
+function presentWrap(string) {
+  let dimensions = string.split('x');
+  // [ '43', '3', '21' ]
+
+  let length = dimensions[0];
+  let width = dimensions[1];
+  let height = dimensions[2];
+
+  let extra = Math.min((length * width), (width * height), (height * length));
+
+  let total = (2 * length * width) + (2 * width * height) + (2 * height * length) + extra;
+  console.log(total);
+  return total;
+}
+
+function listLoop() {
+  let totalWrap = 0;
+  for (var i = 0; i < INPUT.length - 1; i++) {
+    totalWrap += presentWrap(INPUT[i])
+  }
+  console.log(totalWrap);
+}
+
+listLoop()
